1d123416058aabd3545bc24b6e7900cb49bdab03
Nippe <niklaspettersson92@hotmail.com>
2025-09-11 11:58:52 +0200

    SCRUM-1 hotfix again

--- backend/app.py
+++ backend/app.py
@@ -101,4 +101,6 @@ def weather():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
